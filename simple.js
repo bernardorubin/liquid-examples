c2c1767b0616de9c209f39e77d5d6938cc23d9ec
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-28 17:54:01 -0600

    Liquid Sort Filter

--- simple.js
+++ simple.js
@@ -2,5 +2,10 @@ const Liquid = require('liquidjs')
 const engine = new Liquid()
 
 engine
-  .parseAndRender('{{name | capitalize}}', { name: 'lucy' })
+  .parseAndRender('{{name | capitalize}}', { name: 'bernardo' })
   .then(console.log)
+
+nums = [5, 3, 2, 4, 1]
+ctx = { data: nums }
+
+engine.parseAndRender('Sorted data: {{ data | sort }}', ctx).then(console.log)
