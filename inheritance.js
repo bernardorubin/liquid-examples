a13150587b4829118f0a18c5f5a7dc1f72b5fdec
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-28 18:48:26 -0600

    Liquid Inheritance

---
+++ inheritance.js
@@ -0,0 +1,9 @@
+const Liquid = require('liquidjs')
+const path = require('path')
+
+const engine = new Liquid({
+  root: path.resolve(__dirname, 'views/'),
+  extname: '.liquid'
+})
+
+engine.renderFile('derived', { content: 'Some content' }).then(console.log)
