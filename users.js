9c3755e033d4d6c83f9913dbe4d513e020267163
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-28 18:34:06 -0600

    Liquid for tag

---
+++ users.js
@@ -0,0 +1,12 @@
+// Liquid for tag
+const Liquid = require('liquidjs')
+const path = require('path')
+
+const engine = new Liquid({
+  root: path.resolve(__dirname, 'views/'),
+  extname: '.liquid'
+})
+
+ctx = { users: [{ name: 'Peter', age: 24 }, { name: 'Lucy', age: 34 }] }
+
+engine.renderFile('users', ctx).then(console.log)
