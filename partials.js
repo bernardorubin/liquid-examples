1967c15d18f05f35b5e2be82af68ba117ec99fe9
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2019-03-28 18:39:10 -0600

    Liquid partials

---
+++ partials.js
@@ -0,0 +1,11 @@
+const Liquid = require('liquidjs')
+const path = require('path')
+
+const engine = new Liquid({
+  root: path.resolve(__dirname, 'views/'),
+  extname: '.liquid'
+})
+
+ctx = { name: 'Peter' }
+
+engine.renderFile('home', ctx).then(console.log)
